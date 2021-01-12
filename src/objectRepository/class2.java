6eef4a38c34b88dce43462e5669ccc246e186990
ANILAK24 <64995554+ANILAK24@users.noreply.github.com>
2021-01-12 16:17:16 -0500

    Update class2.java

--- src/objectRepository/class2.java
+++ src/objectRepository/class2.java
@@ -5,8 +5,7 @@ public class class2 {
 	public void getData()
 	{System.out.println("How are you");
 	
-	System.out.println("Changes to commit");
-	 System.out.println("More Changes to commit");
+	
 	 System.out.println("More Changes to commit to master branches");
 	}
 	public static void main(String[] args) {
