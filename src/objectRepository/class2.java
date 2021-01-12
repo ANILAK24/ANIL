2dc3aeb31a68b0ede72dbb5d6ee6574886d7e2b1
ANILAK24 <64995554+ANILAK24@users.noreply.github.com>
2021-01-12 13:59:40 -0500

    Update class2.java

--- src/objectRepository/class2.java
+++ src/objectRepository/class2.java
@@ -3,7 +3,8 @@ package objectRepository;
 public class class2 {
 
 	public void getData()
-	{}
+	{system.out.println("How are you")
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
