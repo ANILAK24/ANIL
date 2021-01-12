2294809fddbd93ecd0f7857d6844b379314ff890
ANILAK24 <64995554+ANILAK24@users.noreply.github.com>
2021-01-12 15:41:44 -0500

    Update class2.java

--- src/objectRepository/class2.java
+++ src/objectRepository/class2.java
@@ -6,6 +6,7 @@ public class class2 {
 	{System.out.println("How are you");
 	
 	System.out.println("Changes to commit");
+	 System.out.println("More Changes to commit");
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
